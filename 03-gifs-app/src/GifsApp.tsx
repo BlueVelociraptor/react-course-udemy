24bcd72e502a604ca7f4088bb4652bea3fafc8e9
BlueVelociraptor <bluevelociraptor1555@gmail.com>
2026-01-23 02:37:25 -0500

    Sección #6 completada

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import { GifsList } from "./gifs/components/GifsList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
-import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
-
-const searches: string[] = ["Jack Calavera", "Interrogacion", "Shellcol GG"];
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
+  const [previousTerms, setPreviousTerms] = useState(["jack calavera"]);
+  const [gifs, setGifs] = useState<Gif[]>([]);
+
+  const handleTermClicked = async (term: string) => {
+    const gifsList = await getGifsByQuery(term);
+    setGifs(gifsList);
+  };
+
+  const handleSearch = async (query: string) => {
+    query = query.toLowerCase().trim();
+
+    if (previousTerms.includes(query)) {
+      return;
+    }
+
+    if (previousTerms.length === 8) {
+      return;
+    }
+
+    if (query) {
+      setPreviousTerms((value) => [query, ...value]);
+
+      const gifsList = await getGifsByQuery(query);
+      setGifs(gifsList);
+    }
+  };
+
   return (
     <>
       {/* Header */}
@@ -17,14 +44,17 @@ export const GifsApp = () => {
       />
 
       {/* Search */}
-      <SearchBar inputPlaceholder="Buscar GIFs" />
+      <SearchBar onQuerySearch={handleSearch} inputPlaceholder="Buscar GIFs" />
 
       {/* Busquedas Previas */}
-      <PreviousSearches searches={searches} />
+      <PreviousSearches
+        searches={previousTerms}
+        onLabelClicked={handleTermClicked}
+      />
 
       {/* Gifs */}
 
-      <GifsList GIFs={mockGifs} />
+      <GifsList GIFs={gifs} />
     </>
   );
 };
--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,12 +1,47 @@
+import { useEffect, useState } from "react";
+
 interface Props {
   inputPlaceholder?: string;
+
+  onQuerySearch: (search: string) => void;
 }
 
-export const SearchBar = ({ inputPlaceholder = "Buscar" }: Props) => {
+export const SearchBar = ({
+  inputPlaceholder = "Buscar",
+  onQuerySearch,
+}: Props) => {
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onQuerySearch(query);
+    }, 700);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [query]);
+
+  const handleSearch = () => {
+    onQuerySearch(query);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="search-container">
-      <input type="text" placeholder={inputPlaceholder} />
-      <button>Buscar</button>
+      <input
+        type="text"
+        placeholder={inputPlaceholder}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleSearch}>Buscar</button>
     </div>
   );
 };
